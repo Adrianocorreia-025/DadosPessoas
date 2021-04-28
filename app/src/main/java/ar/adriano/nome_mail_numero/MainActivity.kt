f6a95fb307459b368688c625c048e986ad65a744
Adrianocorreia-025 <82658411+Adrianocorreia-025@users.noreply.github.com>
2021-04-28 10:56:15 +0100

    Commit Inicial

---
+++ app/src/main/java/ar/adriano/nome_mail_numero/MainActivity.kt
@@ -0,0 +1,11 @@
+package ar.adriano.nome_mail_numero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
