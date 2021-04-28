01e6fe33c24ae3ee11249b4c0b3a8bb77af53f3d
Adrianocorreia-025 <82658411+Adrianocorreia-025@users.noreply.github.com>
2021-04-28 11:12:55 +0100

    Nova tela Para receber mensagens

---
+++ app/src/main/java/ar/adriano/nome_mail_numero/ReceberSmS.kt
@@ -0,0 +1,11 @@
+package ar.adriano.nome_mail_numero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReceberSmS : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receber_sm_s)
+    }
+}
