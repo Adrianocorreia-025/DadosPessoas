4fd0b7436d523fd2db9674aafe1796b2789ba6f2
Adrianocorreia-025 <82658411+Adrianocorreia-025@users.noreply.github.com>
2021-04-28 11:47:39 +0100

    mensagem mail nome

--- app/src/main/java/ar/adriano/nome_mail_numero/MainActivity.kt
+++ app/src/main/java/ar/adriano/nome_mail_numero/MainActivity.kt
@@ -1,11 +1,53 @@
 package ar.adriano.nome_mail_numero
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun MensageSend(view: View) {
+
+        val editTextNome = findViewById<EditText>(R.id.editTextTextPersonName)
+        val Nome = editTextNome.text.toString()
+        val editTextEmail = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val mail = editTextEmail.text.toString()
+        val editTextNumero = findViewById<EditText>(R.id.editTextPhone)
+        val Numero = editTextNumero.text.toString()
+
+        if(Nome.isBlank()){
+            editTextNome.error = getString(R.string.mensagem_vazia)
+            return
+        }
+
+        if(Nome.isBlank()){
+            editTextEmail.error = getString(R.string.mensagem_vazia)
+            return
+        }
+
+        if(Nome.isBlank()){
+            editTextNumero.error = getString(R.string.mensagem_vazia)
+            return
+        }
+
+        val intent = Intent(this,ReceberSmS::class.java)
+        intent.putExtra(Companion.iNFOnOME, Nome)
+        startActivity(intent)
+
+        val intent_B = Intent(this,ReceberSmS::class.java)
+        intent.putExtra(Companion.iNFOnOME, Nome)
+        startActivity(intent)
+
+
+    }
+
+    companion object {
+        const val iNFOnOME = "NOME"
     }
 }
